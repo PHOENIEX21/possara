bbb8f7807b5bd312e418776018ed27c54371ce2f
PHOENIEX21 <adunoyepaul7@gmail.com>
2026-09-20 08:43:36 +0100

    Remove obsolete feed stylesheet import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./feed-profile-refresh.css";
 import App from "./App.tsx";
 import { isSupabaseConfigured } from "./lib/supabase";
 import { BrandMark } from "./components/BrandMark";
