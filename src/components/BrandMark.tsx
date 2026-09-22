a7f6ddca92d7e3ab152d8ea6aaf8ddf973d91532
PHOENIEX21 <adunoyepaul7@gmail.com>
2026-09-22 02:07:33 +0100

    Rebuild POSSARA logo as two-ribbon reference symbol

--- src/components/BrandMark.tsx
+++ src/components/BrandMark.tsx
@@ -1,16 +1,16 @@
 export function BrandMark({ className = "h-8 w-8" }: { className?: string }) {
   return (
-    <svg viewBox="0 0 48 48" className={className} aria-hidden="true">
+    <svg viewBox="0 0 52 48" className={className} aria-hidden="true">
       <defs>
-        <linearGradient id="possara-top" x1="7" y1="8" x2="40" y2="18" gradientUnits="userSpaceOnUse">
-          <stop stopColor="#8B28F2"/><stop offset=".46" stopColor="#E33AD1"/><stop offset="1" stopColor="#FF9B43"/>
+        <linearGradient id="possara-top" x1="6" y1="7" x2="46" y2="20" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#7C2CF3"/><stop offset=".42" stopColor="#D637E2"/><stop offset=".72" stopColor="#FF5B8C"/><stop offset="1" stopColor="#FFA33E"/>
         </linearGradient>
-        <linearGradient id="possara-lower" x1="9" y1="18" x2="31" y2="39" gradientUnits="userSpaceOnUse">
-          <stop stopColor="#A82DF3"/><stop offset=".5" stopColor="#7C35F1"/><stop offset="1" stopColor="#5363F4"/>
+        <linearGradient id="possara-lower" x1="7" y1="18" x2="34" y2="43" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#D72DEB"/><stop offset=".48" stopColor="#A330F1"/><stop offset="1" stopColor="#6436E9"/>
         </linearGradient>
       </defs>
-      <path d="M9 7.5h20.4c7.1 0 11.6 3.8 11.6 9.3 0 5.7-4.7 9.5-11.8 9.5H18.5l6.8-7.3h4.2c2.2 0 3.5-.8 3.5-2.2 0-1.3-1.3-2.1-3.5-2.1H9.8A3.8 3.8 0 0 1 6 10.9v-.1a3.3 3.3 0 0 1 3-3.3Z" fill="url(#possara-top)"/>
-      <path d="M6.2 20.1c0-2 1.6-3.6 3.6-3.6h13.8l-6.9 7.4h-2.5v5.7h8.2l-6.8 7.4H9.8a3.6 3.6 0 0 1-3.6-3.6V20.1Z" fill="url(#possara-lower)"/>
+      <path d="M10 6.5h23.5c7 0 12 4.1 12 9.7 0 5.5-5 9.7-12 9.7H19.3l7.6-8.2h6.6c1.8 0 2.8-.6 2.8-1.6 0-.9-1-1.5-2.8-1.5H10c-2.9 0-5-1.7-5-4.1s2.1-4 5-4Z" fill="url(#possara-top)"/>
+      <path d="M5 18.4h20.7c3.7 0 6.3 2 6.3 4.8 0 1.6-.8 3-2.4 4.4L15.8 40.5c-1.5 1.4-3.3 2.1-5.2 2.1-3.2 0-5.6-2-5.6-4.8V18.4Zm9.2 8.2v5.6l6.1-5.6h-6.1Z" fill="url(#possara-lower)"/>
     </svg>
   );
 }
